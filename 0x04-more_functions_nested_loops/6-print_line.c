21acc543b4e7fd6a867ef319fda4918703173f7d
Yumvagusenga <yumvagusengajaphet@gmail.com>
2023-08-17 17:03:00 -0700

    C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,27 +1,22 @@
 #include "main.h"
 
 /**
- * print_diagonal - printing lines diagonal
- * @n: input variable
+ * print_line - printing lines
+ * @n: number of times
  *
  * Return: void
  */
 
 
-void print_diagonal(int n)
+void print_line(int n)
 {
-int i, j;
+int i;
 
-if (n <= 0)
-_putchar('\n');
-for (i = 0; i < n; i++)
+i = 0;
+while (i < n)
 {
-for (j = 0; j < i; j++)
-{
-_putchar(' ');
+_putchar('_');
+i++;
 }
-_putchar('\\');
 _putchar('\n');
-}
-
 }
